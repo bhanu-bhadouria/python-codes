20ee0036b095d8bf20dbefe5abbfd7b75a129fe3
bhanu bhadouria <bhanubhadouria123@gmail.com>
2021-05-29 16:24:03 +0530

    -added layer control

--- APP_2_volcano&population_index/app2_python.py
+++ APP_2_volcano&population_index/app2_python.py
@@ -8,6 +8,9 @@ list_lat=list(data["LAT"])
 list_lon=list(data["LON"])
 list_elev=list(data["ELEV"])
 list_name=list(data["NAME"])
+fgp=folium.FeatureGroup(name="population")
+fgp.add_child(folium.GeoJson(data=open("world.json","r",encoding="utf-8-sig").read(),style_function=lambda x: {"fillColor":"green" if x["properties"]["POP2005"]<10000000 else "orange" if 10000000 < x["properties"]["POP2005"] < 20000000 else "red"}))
+fgv=folium.FeatureGroup(name="volcanoes")
 for lat,lon,elev,name in zip(list_lat,list_lon,list_elev,list_name):
     if elev > 3500:
         color="red"
